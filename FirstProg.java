fdf0e72cd1d0dbb70fbcb53ab6f49900a324986f
Jennifer Veigel <jenveigel@Jennifers-MacBook-Pro.local>
2017-09-18 14:22:29 -0600

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+    public static void main( String[] args ) {
+        System.out.println( "I, Jen Veigel, am determined to learn how to code." );
+        System.out.println( "Today's date is 15 September 2017." );
+    }
+}
