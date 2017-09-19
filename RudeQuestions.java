c787e5c85bd059d99a5b2e59ce1d3873450483ad
Your Name <you@example.com>
2017-09-19 16:14:59 -0600

    add exercise 8

--- RudeQuestions.java
+++ RudeQuestions.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class RudeQuestions {
   public static void main( String[] args ) {
-    String name;
+    String name, religion;
     int age;
     double weight, income;
 
@@ -10,17 +10,28 @@ public class RudeQuestions {
 
     System.out.print( "Hello. What is your name?" );
     name = keyboard.next();
+    //unable to blow up the program here
+    //numbers and symbols can be part of a String of characters
 
     System.out.print( "Hi, " + name + "! How old are you? " );
     age = keyboard.nextInt();
+    //letters and numbers wtih decimals blow up the program here since neither is an integer
 
     System.out.println( "So you're " + age + ", eh? That's not very old." );
     System.out.print( "How much do you weigh, " + name + "? " );
     weight = keyboard.nextDouble();
+    /* entering an integer here does not blow up the program
+    because integers are included in doubles. Not vice-versa.*/
 
     System.out.println( weight + "! Better keep that quiet!!" );
+
+    System.out.print( "Just kidding! What religion are you? " );
+    religion = keyboard.next();
+
     System.out.print("Finally, what's your income, " + name + "? " );
     income = keyboard.nextDouble();
+    //only letters and symbols (other than decimal) blow up the program here
+    //integers and fractions expressed with decimals work--positive and negative
 
     System.out.print( "Hopefully that is " + income + " per hour" );
     System.out.println( " and not per year!" );
