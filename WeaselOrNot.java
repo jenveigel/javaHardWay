35170e87d9181a0a1f4d04a6b5795125f1670e05
Your Name <you@example.com>
2017-09-19 22:11:01 -0600

    add exercise 13

---
+++ WeaselOrNot.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class WeaselOrNot {
+  public static void main( String[] args ) {
+    Scanner keyboard = new Scanner(System.in);
+    String word;
+    boolean yep, nope;
+
+    System.out.println( "Type the word \"weasel\", please." );
+    word = keyboard.next();
+
+    yep = "weasel".equals(word);
+    nope = ! "weasel".equals(word);
+    /* also works with
+    ... word.equals("weasel");
+    ... ! word.equals("weasel"); */
+
+    System.out.println( "You typed what was requested: " + yep );
+    System.out.println( "You ignored polite instructions: " + nope );
+
+  }
+}
